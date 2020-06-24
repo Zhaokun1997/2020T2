501a341c00e896daac81558280836b2bd5e849bc
Zhaokun1997 <357610140@qq.com>
2020-06-24 10:03:04 +0800

    lab04 finished

---
+++ comp9044/labs/week4/lab04/shuffle_test2.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+
+
+
+
+
+RANDOM=$$
+s=$RANDOM
+RANDOM=$$
+size=$(($RANDOM % 300))
+e=$(($size + $s))
+
+
+
+while test $s -le $e
+do
+   lines="$s"$'\n'"$lines";
+   s=$(expr $s + 1);
+   
+done
+t=$(echo "$lines" | sed "/^$/d"|./shuffle.pl|sort)
+r=$(echo "$lines" | sed "/^$/d"|sort)
+
+if test "$t" = "$r" 
+then
+   echo "pass"
+else
+
+   echo "fail"
+fi
+
+
+
+
+
+
+
+
+
